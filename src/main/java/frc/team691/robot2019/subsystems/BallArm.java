d47f11320b34d1b988f96da83f2cf2f0313112b0
Gabriel Totev <gttotev8@gmail.com>
2019-02-15 16:20:37 -0800

    Finish stable testing BallArm subsystem

--- src/main/java/frc/team691/robot2019/subsystems/BallArm.java
+++ src/main/java/frc/team691/robot2019/subsystems/BallArm.java
@@ -8,7 +8,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.team691.robot2019.commands.StickArm;
 
 public class BallArm extends Subsystem {
-    private static final double MOTOR_HOLD_OUT = 0.2;
+    private static double MOTOR_LOWER_MAX_OUT = 1;
+    private static double MOTOR_UPPER_MAX_OUT = 1;
+    private static double MOTOR_HOLD_OUT = 0.2;
 
     private static BallArm instance;
 
@@ -19,12 +21,20 @@ public class BallArm extends Subsystem {
         return instance;
     }
 
-    private Spark lowerMotor = new Spark(0);
-    private Spark upperMotor = new Spark(1);
+    private Spark lowerMotor = new Spark(1);
+    private Spark upperMotor = new Spark(0);
     private Encoder lowerEnc = new Encoder(8, 9);
-    private Encoder upperEnc;
+    private Encoder upperEnc = new Encoder(6, 7);
 
     private int encGoal = 0;
+
+    private BallArm() {
+        lowerMotor.setInverted(true);
+        upperMotor.setInverted(true);
+        SmartDashboard.putNumber("lowerMotorMax", SmartDashboard.getNumber("lowerMotorMax", 1));
+        SmartDashboard.putNumber("upperMotorMax", SmartDashboard.getNumber("upperMotorMax", 1));
+        SmartDashboard.putNumber("motorHoldOut", SmartDashboard.getNumber("motorHoldOut", 0.2));
+    }
 
     @Override
     public void initDefaultCommand() {
@@ -36,24 +46,28 @@ public class BallArm extends Subsystem {
         SmartDashboard.putNumber("lowerMotor", lowerMotor.get());
         SmartDashboard.putNumber("upperMotor", upperMotor.get());
         SmartDashboard.putNumber("lowerEnc", lowerEnc.get());
+        SmartDashboard.putNumber("upperEnc", upperEnc.get());
     }
 
     public void driveStop() {
         lowerMotor.stopMotor();
         upperMotor.stopMotor();
+        MOTOR_LOWER_MAX_OUT = SmartDashboard.getNumber("lowerMotorMax", 1);
+        MOTOR_UPPER_MAX_OUT = SmartDashboard.getNumber("upperMotorMax", 1);
+        MOTOR_HOLD_OUT = SmartDashboard.getNumber("motorHoldOut", 0.2);
     }
 
     public void driveHold() {
         driveStop();
         int error = encGoal - lowerEnc.get();
-        if (Math.abs(error) > 4) {
+        if (Math.abs(error) > 2) {
             lowerMotor.set(Math.copySign(MOTOR_HOLD_OUT, error));
         }
     }
 
     public void driveStick(Joystick stick) {
-        lowerMotor.set(stick.getY());
-        upperMotor.set(0);
+        lowerMotor.set(stick.getY() * MOTOR_LOWER_MAX_OUT);
+        upperMotor.set(stick.getX() * MOTOR_UPPER_MAX_OUT);
         encGoal = lowerEnc.get();
     }
 }
